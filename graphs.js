86b4d4fcd3ada2fc745a4491067f181372f2b67c
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2019-01-09 12:52:44 +0100

    changed data loading into promise

--- graphs.js
+++ graphs.js
@@ -1,8 +1,30 @@
 window.onload = function() {
-  loadshp({
-      url: 'Gemeente2016.zip', // path or your upload file
-      encoding: 'big5' // default utf-8
-  }, function(geojson) {
-      console.log(geojson)
+  let gemeenteShape = new Promise(function(resolve, reject) {
+    loadshp({
+        url: 'data/Gemeente2016.zip', // path or your upload file
+        encoding: 'big5' // default utf-8
+    }, function(geojson) {
+      resolve(geojson);
+    });
+  })
+
+  let wijkShape = new Promise(function(resolve, reject) {
+    loadshp({
+        url: 'data/Wijk2016.zip', // path or your upload file
+        encoding: 'big5' // default utf-8
+    }, function(geojson) {
+      resolve(geojson);
+    });
+  })
+
+  let requests = [gemeenteShape, wijkShape, d3.json('data/Gemeente/score_gemeente.json'), d3.json('data/Wijk/wijk.json')];
+
+  Promise.all(requests).then(function(response) {
+    console.log(response)
   });
+}
+
+function mapClick() {
+  document.getElementById("lineChart").innerHTML = "A line diagram appears here"
+  document.getElementById("back").innerHTML = "<button type='button' onclick='toCountry()'>Back</button>"
 }
