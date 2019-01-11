59fa2a84994ff058def8bf0185cf94e906b37df2
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2019-01-11 15:29:30 +0100

    now using an external svg

--- PROCESS.md
+++ PROCESS.md
@@ -13,3 +13,9 @@ A json file would be easier to use than a xlsx file. Conversions were made.
 # Day 4
 The script I found for converting shapefiles to geojson in javascript could not be used. The script works fine with one shapefile, but loading two shapefiles at the same time causes one of the resulting geojsons to be overwritten somehow. There is actually no reason for me to preprocess when my script is run, so I have converted them using PyShp and saved them as json files.
 The preprocessed geojsons are too large for github, so I zipped them and found a way to unzip them in my script.
+
+# Day 5
+Made an attempt at creating a map. I got a square that makes my site unresponsive.  
+I am thinking of converting the geojson to a topojson as many examples use this format. If I were to use topojson, I would not have to zip and unzip the geojson.  
+Tried the conversion, but there is no difference. After multiple attempts at creating the map, I came to the conclusion that it would not work.  
+Now I will download a .svg from wikipedia and add it to my html and alter it if possible for my own purposes.
--- graphs.js
+++ graphs.js
@@ -11,21 +11,62 @@ window.onload = function() {
       return JSZip.loadAsync(data);
   });
 
-  let requests = [d3.json('data/Gemeente/score_gemeente.json'), d3.json('data/Wijk/wijk.json'), shapesZip];
+  let requests = [d3.json('data/Gemeente/score_gemeente.json'), d3.json('data/Wijk/wijk.json'), shapesZip)];
 
   Promise.all(requests).then(function(response) {
     console.log(response);
-    var y = response[2].file("WijkShapes.json").async("string");
-    var z = response[2].file("GemeenteShapes.json").async("string");
 
-    console.log("unzipping JSZip");
-    Promise.all([y, z]).then(function(resp) {
-      var wijken = JSON.parse(resp[0]);
-      var gemeentes = JSON.parse(resp[1]);
-      console.log(wijken);
-      console.log(gemeentes);
-    });
+    // var y = response[2].file("WijkShapes.json").async("string");
+    // var z = response[2].file("GemeenteShapes.json").async("string");
+
+    // console.log("unpacking JSZip");
+    // Promise.all([y, z]).then(function(resp) {
+    //   var wijken = JSON.parse(resp[0]);
+    //   var gemeentes = JSON.parse(resp[1]);
+    //   console.log(wijken);
+    //   console.log(gemeentes);
+    // });
   });
+}
+
+function createMap(data) {
+  // width and height
+  var w = 600,
+      h = 700;
+
+  // var geojson = topojson.feature(data, data.objects.GemeenteShapes)
+  // console.log(geojson)
+
+  // define map projection
+  var projection = d3.geoMercator().scale(6700)
+
+  //Define default path generator
+  var path = d3.geoPath()
+    .projection(projection);
+
+  var b = path.bounds(data.features[3]),
+  s = .2 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
+  t = [(w - s * (b[1][0] + b[0][0])) / 2, (h - s * (b[1][1] + b[0][1])) / 2];
+
+  projection
+          .scale(s * 4000000)
+          .translate(t);
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("id", "map")
+    .attr("width", w)
+    .attr("height", h)
+
+  svg.selectAll("path")
+          .data(data.features)
+          .enter()
+          .append("path")
+          .attr("d", path)
+          .style("fill", "none")
+          .style("stroke", "black");
+
+  console.log("done")
 }
 
 function mapClick() {
